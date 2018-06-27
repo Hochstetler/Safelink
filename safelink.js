40c5c87d21e6298d32fde35e31cc9c8b851e4a89
Hochstetler <30820354+Hochstetler@users.noreply.github.com>
2018-06-27 03:24:02 +0000

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -131,7 +131,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "http://song.gadgetsyn.com/link?url=" + encryptedUrl;
+            a_to_ve[i].href = "http://song.gadgetsyn.com/safelink/safelink.php?go=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
